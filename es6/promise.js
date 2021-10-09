8b74c5c13652ad290969e8402166d28983b7a8b0
heizi <hutao06051@126.com>
2021-10-09 23:48:04 +0800

    feat: udpate

--- es6/promise.js
+++ es6/promise.js
@@ -30,3 +30,39 @@ await 常量； 相当于 Promise.resolve(4)
 try {await 异步方法()} catch (e) {console.error(e)} 
 
 */
+
+const ajaxUrl1 = 'https://jsonplaceholder.typicode.com/todos/1'
+const ajaxUrl2 = 'https://jsonplaceholder.typicode.com/todos/2'
+const ajaxUrl3 = 'https://jsonplaceholder.typicode.com/todos/3'
+
+// const p1 = fetch(ajaxUrl1)
+// const p2 = fetch(ajaxUrl2)
+// const p3 = fetch(ajaxUrl3)
+
+// const ps = [p1,p2,p3]
+
+async function f() {
+	// const a = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+	// const b = await fetch('https://jsonplaceholder.typicode.com/todos/2');
+	// const p1 = fetch(ajaxUrl1)
+	// const p2 = fetch(ajaxUrl2)
+	// const [a, b] = await Promise.all([p1, p2])
+	// await for(let i of ps) {
+	// 	i()
+	// }
+	for await(let i of [1,2,3]) {
+		 fetch(`https://jsonplaceholder.typicode.com/todos/${i}`)
+	}
+}
+f()
+
+// 使用 for 循环批量、串行执行 。不能使用 forEach 和 map，因为直接它们直接返回了。
+// 并行的话，需要使用 Promise.all 或者 for await 
+
+
+
+
+
+
+
+
